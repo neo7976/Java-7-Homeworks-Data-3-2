ccaad0615711809669c06ab19f52a0be01b1d805
Dmitrii Sobin <neo7976@yandex.ru>
2023-01-21 15:10:28 +0300

    Второй entity написан со связью на первую таблицу

--- src/main/java/com/example/java7homeworksdata32/entity/Order.java
+++ src/main/java/com/example/java7homeworksdata32/entity/Order.java
@@ -1,4 +1,39 @@
 package com.example.java7homeworksdata32.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.boot.actuate.autoconfigure.metrics.MetricsProperties;
+
+import javax.persistence.*;
+import javax.validation.constraints.Min;
+import java.sql.Time;
+
+@Entity
+@Table(name = "orders")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false)
+    private Time data;
+
+    @OneToOne(optional = false)
+//    @Column(nullable = false, name = "customers_id")
+    private Customer customer;
+
+    @Column(nullable = false, name = "product_name")
+    private String productName;
+
+    @Column(nullable = false)
+    @Min(1)
+    private int amount;
+
+
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,26 +1,26 @@
-insert into homework4.CUSTOMERS(name, surname, age, phone_number)
+insert into customers(name, surname, age, phone_number)
 values ('Ivan', 'Корнеев', 24, '9857851279');
-insert into homework4.CUSTOMERS(name, surname, age, phone_number)
+insert into customers(name, surname, age, phone_number)
 values ('Alex', 'Захаров', 36, '9857981279');
-insert into homework4.CUSTOMERS(name, surname, age, phone_number)
+insert into customers(name, surname, age, phone_number)
 values ('Petya', 'Грачев', 18, '9898851279');
-insert into homework4.CUSTOMERS(name, surname, age, phone_number)
+insert into customers(name, surname, age, phone_number)
 values ('Alex', 'Родионов', 27, '9857851479');
-insert into homework4.CUSTOMERS(name, surname, age, phone_number)
+insert into customers(name, surname, age, phone_number)
 values ('Olga', 'Алексеева', 74, '9857851285');
-insert into homework4.CUSTOMERS(name, surname, age, phone_number)
+insert into customers(name, surname, age, phone_number)
 values ('Sveta', 'Васильева', 25, '9851051279');
-insert into homework4.CUSTOMERS(name, surname, age, phone_number)
+insert into customers(name, surname, age, phone_number)
 values ('Nicolay', 'Шишкин', 15, '9857851479');
-insert into homework4.CUSTOMERS(name, surname, age, phone_number)
+insert into customers(name, surname, age, phone_number)
 values ('Stas', 'Яковлев', 16, '9857861279');
-insert into homework4.CUSTOMERS(name, surname, age, phone_number)
+insert into customers(name, surname, age, phone_number)
 values ('Vlad', 'Грачев', 34, '9857852679');
-insert into homework4.CUSTOMERS(name, surname, age, phone_number)
+insert into customers(name, surname, age, phone_number)
 values ('Igor', 'Козлов', 28, '9857851256');
 
 
-insert into homework4.ORDERS(date, customers_id, product_name, amount)
+insert into orders(date, customers_id, product_name, amount)
 values ('2023.01.10 10.15.10', 7, 'Молоко', 4);
 insert into homework4.ORDERS(date, customers_id, product_name, amount)
 values ('2023.01.10 10.16.10', 5, 'Хлеб', 1);
